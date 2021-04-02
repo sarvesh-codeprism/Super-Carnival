f634c35c88aa48b0a7457b9d49fb58557e1016da
Sarvesh Patil <sarveshspatil111@gmail.com>
2021-04-02 11:33:16 +0530

    Add 20.valid_palindrome.py

---
+++ 20.valid_palindrome.py
@@ -0,0 +1,24 @@
+# Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+ 
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+
+# Example 2:
+
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+
+# Solution 1 -- Time complexity : O(n) -- Space complexity : O(n)
+class Solution_1:
+    def isPalindrome(s):
+        st = ''
+        for ch in s:
+            if ch.isalnum():
+                st += ch.lower()
+        return st == st[::-1]
